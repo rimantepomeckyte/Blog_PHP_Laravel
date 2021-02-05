7bdf35d44f1a681d280a6156300ca9d36b6d1f2a
rimantepomeckyte <rimante.pomeckyte@gmail.com>
2021-02-05 16:38:23 +0200

    with add post form

--- resources/views/blog_theme/pages/add-post.blade.php
+++ resources/views/blog_theme/pages/add-post.blade.php
@@ -3,32 +3,32 @@ metoda susikurti addPost kaip suprantu BlogControllery-->
 @extends('blog_theme/main')
 @section('content')
     <div class="row justify-content-center mb-5">
-        <h2>Naujas įrašas</h2>
+        <h2>New Post</h2>
     </div>
     <form method="post">
         <div class="form-group">
-            <label for="title">Pavadinimas</label>
-            <input type="title" class="form-control" id="title" placeholder="Įrašo pvadinimas">
+            <label for="title">Title</label>
+            <input type="title" class="form-control" id="title" placeholder="Post title">
         </div>
         <div class="form-group">
-            <label for="category">Kategorija</label>
+            <label for="category">Category</label>
             <select class="form-control" id="category">
-                <option value="" disabled selected>Pasirinkite kategoriją</option>
+                <option value="" disabled selected>Choose category</option>
                 @foreach($options as $option)
                     <option value={{$option}}>{{$option}}</option>
                 @endforeach
             </select>
         </div>
         <div class="form-group">
-            <label for="content">Jūsų įrašas:</label>
+            <label for="content">Your post content:</label>
             <textarea class="form-control" id="content" rows="5"></textarea>
         </div>
         <div class="form-group">
-            <label for="upload">Pasirinkti paveikslėlį:</label>
+            <label for="upload">Choose image:</label>
             <input type="file" class="form-control" id="upload">
         </div>
         <div class="form-group d-flex justify-content-center m-5">
-            <button type="submit" class="btn btn-secondary rounded">Paskelbti</button>
+            <button type="submit" class="btn btn-info rounded">Post</button>
         </div>
     </form>
 @endsection

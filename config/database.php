d843fd723c08f2b9c682dd249f32980eca40a2b6
rimantepomeckyte <rimante.pomeckyte@gmail.com>
2021-02-10 21:30:33 +0200

    updated database

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('DATABASE_URL');
+
 return [
 
     /*
@@ -14,8 +16,9 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
+    'default' => env('DB_CONNECTION', 'pqsql'),
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+   // 'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -81,11 +84,16 @@ return [
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            //'host' => env('DB_HOST', 'localhost'),
+            'host' => $DATABASE_URL['host'],
+            'port' => $DATABASE_URL['port'],
+            //'port' => env('DB_PORT', '1433'),
+            //'database' => env('DB_DATABASE', 'forge'),
+            'database' => ltrim($DATABASE_URL['path'], "/"),
+            //'username' => env('DB_USERNAME', 'forge'),
+            'username' => $DATABASE_URL['user'],
+            'password' =>  $DATABASE_URL['pass'],
+            //'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
